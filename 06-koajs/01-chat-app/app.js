11c0722fad172ec29d98dd8715dfcd2fd70ae3f1
Vera Lobacheva <v.lobacheva@hh.ru>
2021-07-16 17:07:40 +0300

    add solution for 6-module 1-task

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,30 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const subscribers = new Set();
+const getDataPromise = () => {
+    return new Promise((resolve, reject) => {
+        subscribers.add(resolve);
+    });
+};
+const setDataPromisesResult = (result) => {
+    subscribers.forEach((resolveCallback) => resolveCallback(result));
+    subscribers.clear();
+};
+
 router.get('/subscribe', async (ctx, next) => {
+    ctx.status = 200;
+    ctx.body = await getDataPromise();
 });
 
 router.post('/publish', async (ctx, next) => {
+    const message = ctx.request.body.message;
+    if (!message) {
+        ctx.throw(400, 'Message is empty');
+    }
+    setDataPromisesResult(message);
+    ctx.status = 200;
+    ctx.body = 'Published';
 });
 
 app.use(router.routes());
