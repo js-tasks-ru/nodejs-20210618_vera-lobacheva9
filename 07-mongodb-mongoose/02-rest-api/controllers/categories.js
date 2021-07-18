95bb5ac73f95f2a48f0a662af83dcd61703ad976
Vera Lobacheva <v.lobacheva@hh.ru>
2021-07-18 22:57:59 +0300

    add solution for 7-module 2-task

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,22 @@
+const Category = require('../models/Category');
+
+const subCategoryDTO = (doc) => {
+  return {
+    id: doc._id,
+    title: doc.title,
+  }
+}
+const categoryDTO = (doc) => {
+  return {
+    id: doc._id,
+    title: doc.title,
+    subcategories: doc.subcategories.map(subCategoryDTO),
+  }
+}
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find();
+  ctx.body = {
+    categories: categories.map(categoryDTO),
+  };
 };
